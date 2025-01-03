b41beae0839435a9e2b3f957ddefb82a60c478c6
Alvino Barboza <alvinobarboza@gmail.com>
2025-01-03 12:26:48 -0300

    Debug

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const folderHandler = require('./util/folderHandler');
-const { getToken } = require('./util/moTVCalls');
 const {
     getActiveCustomersTIP,
     getActiveCustomersSLZ,
@@ -8,20 +7,39 @@ const {
 } = require('./util/reportsEPG');
 const writeToReport = require('./util/writeToFile');
 
-Promise.all([
-    getColombiaYplay(),
-    getActiveCustomersTIP(),
-    getActiveCustomersSLZ(),
-    getActiveCustomersOOPS(),
-])
-    .then((data) => {
+// Promise.all([
+//     getColombiaYplay(),
+//     getActiveCustomersTIP(),
+//     getActiveCustomersSLZ(),
+//     getActiveCustomersOOPS(),
+// ])
+//     .then((data) => {
+//         const yplay = require('./epg.json');
+//         const colombia = data[0].response.rows;
+//         const tip = data[1].response.rows;
+//         const slz = data[2].response.rows;
+//         const oops = data[3].response.rows;
+
+//         // folderHandler();
+//         // writeToReport(yplay, tip, slz, oops, colombia);
+//     })
+//     .catch((e) => console.log(e));
+
+(async function () {
+    try {
         const yplay = require('./epg.json');
-        const colombia = data[0].response.rows;
-        const tip = data[1].response.rows;
-        const slz = data[2].response.rows;
-        const oops = data[3].response.rows;
+        const colombiaBody = await getColombiaYplay();
+        const colombia = colombiaBody.response.rows;
+        const tipBody = await getActiveCustomersTIP();
+        const tip = tipBody.response.rows;
+        const slzBody = await getActiveCustomersSLZ();
+        const slz = slzBody.response.rows;
+        const oopsBody = await getActiveCustomersOOPS();
+        const oops = oopsBody.response.rows;
 
         folderHandler();
         writeToReport(yplay, tip, slz, oops, colombia);
-    })
-    .catch((e) => console.log(e));
+    } catch (error) {
+        console.log(error);
+    }
+})();
